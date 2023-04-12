0841788024cdfad738d86a0a631a1d0e0bb0add8
unknown <thanhthien2003>
2023-04-12 18:07:42 +0700

    Message

---
+++ module2 Baitap/Module02/src/ss8_lam_lai/CleanCode.java
@@ -0,0 +1,38 @@
+package ss8_lam_lai;
+
+public class CleanCode {
+    public int sum(int num1, int num2, int num3) {
+        int sum = num1 + num2 + num3;
+        return sum;
+    }
+
+    public boolean isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getDayOfMonth(int month, int year) {
+        switch (month) {
+            case 1:
+                return 31;
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 30;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+            case 2:
+                return isLeapYear(year) ? 29 : 28;
+            default:
+                return -1;
+        }
+    }
+}
