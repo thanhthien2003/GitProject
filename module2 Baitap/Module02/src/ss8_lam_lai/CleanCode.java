96ce98f5cedb3d1b7053216e200209d1d1dbc624
unknown <thanhthien2003>
2023-04-13 00:53:19 +0700

    Message

--- module2 Baitap/Module02/src/ss8_lam_lai/CleanCode.java
+++ module2 Baitap/Module02/src/ss8_lam_lai/CleanCode.java
@@ -17,18 +17,18 @@ public class CleanCode {
     public int getDayOfMonth(int month, int year) {
         switch (month) {
             case 1:
-                return 31;
             case 3:
             case 5:
             case 7:
             case 8:
             case 10:
             case 12:
-                return 30;
+                return 31;
             case 4:
             case 6:
             case 9:
             case 11:
+                return 30;
             case 2:
                 return isLeapYear(year) ? 29 : 28;
             default:
